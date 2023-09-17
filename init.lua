1f13b87f6ee85273d22951d2c9c04620955b8514
khanh tra <khanh.quang.t@gmail.com>
2023-09-17 12:22:12 +0700

    remove tree-sitter context plugin

--- init.lua
+++ init.lua
@@ -107,7 +107,7 @@ require('packer').startup(function(use)
 	use { "catppuccin/nvim", as = "catppuccin" }
 	-- code comment
 	use 'numToStr/Comment.nvim'
-	use "JoosepAlviste/nvim-ts-context-commentstring"
+	-- use "JoosepAlviste/nvim-ts-context-commentstring"
 
 	-- git plugin
 	use "tpope/vim-fugitive"
@@ -116,7 +116,7 @@ require('packer').startup(function(use)
 	-- terminal inside neovim
 	use "akinsho/toggleterm.nvim"
 
-	use "nvim-treesitter/nvim-treesitter-context"
+	-- use "nvim-treesitter/nvim-treesitter-context"
 
 	use 'sindrets/diffview.nvim'
 
@@ -393,22 +393,6 @@ if vim.g.neovide then
 	vim.g.neovide_cursor_vfx_particle_density = 10
 	vim.opt.guifont = { "Hack NFM", ":h11" }
 end
-
--- nvim treesitter context 
-require'treesitter-context'.setup{
-	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-	max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-	min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-	line_numbers = true,
-	multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-	trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-	mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-	-- Separator between context and content. Should be a single character string, like '-'.
-	-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-	separator = nil,
-	zindex = 20, -- The Z-index of the context window
-	on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-}
 
 -- lsp saga setup
 require('lspsaga').setup({})
